658f88f825250c614af585a1746c6c66ad719028
AnnAngela <naganjue@vip.qq.com>
2024-02-27 17:38:44 +0800

    feat: remove unnecessary fields in package-lock.json
    Resolve #17

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import fs from "fs";
 import timersPromises from "node:timers/promises";
 import Variable from "./Variable.js";
 import spawnChildProcess from "./spawnChildProcess.js";
+import { packageLockHandler } from "./lockfileHandler.js";
 
 const trimBrackets = (str: string) => str.replace(/^\{(.*)\}$/, "$1");
 
@@ -24,7 +25,7 @@ const inputs = {
 
 debug(`inputs: ${JSON.stringify(inputs)}`);
 
-const lockfilePath = path.join(inputs.cwd, inputs.lockfilePath);
+let lockfilePath = path.join(inputs.cwd, inputs.lockfilePath);
 const packageJsonPath = path.join(inputs.cwd, inputs.packageJsonPath);
 const nodeModulesPath = path.join(inputs.cwd, "node_modules");
 console.info("cwd:", inputs.cwd);
@@ -48,6 +49,46 @@ try {
     throw new Error(`package.json "${packageJsonPath}" does not exist.`, {
         cause,
     });
+}
+
+/**
+ * We don't care about leaving the trash in tmp folder since the program will only be run on Github Action's runner.
+ * But I want to keep the code for future reference.
+
+const needToDelete: string[] = [];
+const deleteFiles = () => {
+    for (const path of needToDelete) {
+        try {
+            // eslint-disable-next-line n/no-sync
+            fs.rmSync(path, { recursive: true, force: true });
+        } catch (err) {
+            console.error(`Failed to delete ${path}:`, err);
+        }
+    }
+};
+process.on("exit", deleteFiles);
+process.on("uncaughtException", (err) => {
+    console.error("Uncaught exception", err);
+    deleteFiles();
+    // eslint-disable-next-line n/no-process-exit
+    process.exit(process.exitCode ?? 1);
+});
+process.on("unhandledRejection", (reason, promise) => {
+    console.error("Unhandled promise rejection", promise, reason);
+    deleteFiles();
+    // eslint-disable-next-line n/no-process-exit
+    process.exit(process.exitCode ?? 1);
+});
+
+*/
+const lockfileParsedPath = path.parse(lockfilePath);
+if (lockfileParsedPath.name === "package-lock") {
+    const {
+        // tmpdir,
+        newLockfilePath,
+    } = await packageLockHandler(lockfilePath, lockfileParsedPath);
+    lockfilePath = newLockfilePath;
+    // needToDelete.push(tmpdir);
 }
 
 const variable = new Variable(inputs.cwd, lockfilePath, packageJsonPath, inputs.customVariable);
