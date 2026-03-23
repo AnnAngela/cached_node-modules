d6d4142e8ddf1e140dc5052d892c0a008cc5a269
AnnAngela <naganjue@vip.qq.com>
2026-03-23 10:56:57 +0800

    Revert: fix: refactor fetchFileGitCommitLong to use local git command instead of Octokit
    This reverts commit 1bc367d7025a40575827bee2e26d288b824dd84b.

--- src/Variable.ts
+++ src/Variable.ts
@@ -1,7 +1,8 @@
 import { debug } from "@actions/core";
 import semver from "semver";
-import { hashCalc } from "./hashCalc.js";
 import spawnChildProcess from "./spawnChildProcess.js";
+import { hashCalc } from "./hashCalc.js";
+import octokit from "./Octokit.js";
 
 interface variableMap {
     OS_NAME: string;
@@ -30,10 +31,14 @@ interface variableMap {
 
 const fetchFileGitCommitLong = async (filePath: string) => {
     debug(`[fetchFileGitCommitLong] Fetching git commit long of ${filePath}...`);
-    const gitRoot = process.cwd();
-    debug(`[fetchFileGitCommitLong] gitRoot: ${gitRoot}`);
-    const sha = await spawnChildProcess(`git log -n 1 --format=%H -- ${JSON.stringify(filePath)}`, {
-        cwd: gitRoot,
+    debug(`[fetchFileGitCommitLong] octokit.context.repo: ${JSON.stringify(octokit.context.repo)}`);
+    debug(`[fetchFileGitCommitLong] octokit.context.ref: ${JSON.stringify(octokit.context.ref)}`);
+    debug(`[fetchFileGitCommitLong] octokit.context.sha: ${JSON.stringify(octokit.context.sha)}`);
+    const { data: [{ sha }] } = await octokit.repos.listCommits({
+        ...octokit.context.repo,
+        path: filePath,
+        per_page: 1,
+        sha: octokit.context.ref,
     });
     debug(`[fetchFileGitCommitLong] Fetched git commit long of ${filePath}: ${sha}`);
     return sha;
